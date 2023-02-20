48d02fb7c274a7b278cb8b0715824320da68d25a
lzk23559 <li56897@163.com>
2023-02-20 22:43:21 +0800

    字幕描边3像素 去掉列表循环 修复选源焦点

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -491,9 +491,6 @@ public class PlayActivity extends BaseActivity {
 
     void playUrl(String url, HashMap<String, String> headers) {
         LOG.i("playUrl:" + url);
-        if(autoRetryCount>0 && url.contains(".m3u8")){
-            url="http://home.jundie.top:666/unBom.php?m3u8="+url;
-        }
         String finalUrl = url;
         runOnUiThread(new Runnable() {
             @Override
@@ -742,13 +739,8 @@ public class PlayActivity extends BaseActivity {
             hasNext = mVodInfo.playIndex + 1 < mVodInfo.seriesMap.get(mVodInfo.playFlag).size();
         }
         if (!hasNext) {
-            if(isProgress && mVodInfo!=null){
-                mVodInfo.playIndex=0;
-                Toast.makeText(this, "已经是最后一集了!,即将跳到第一集继续播放", Toast.LENGTH_SHORT).show();
-            }else {
-                Toast.makeText(this, "已经是最后一集了!", Toast.LENGTH_SHORT).show();
-                return;
-            }
+            Toast.makeText(this, "已经是最后一集了!", Toast.LENGTH_SHORT).show();
+            return;
         }else {
             mVodInfo.playIndex++;
         }
