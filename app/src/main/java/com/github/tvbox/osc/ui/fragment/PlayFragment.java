96a6db1198f877219bce1e7a3dc199e4fe47635a
lzk23559 <li56897@163.com>
2023-03-03 19:59:59 +0800

    暂停状态显示视频真实链接

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -1500,6 +1500,7 @@ public class PlayFragment extends BaseLazyFragment {
                         String cookie = CookieManager.getInstance().getCookie(url);
                         if(!TextUtils.isEmpty(cookie))headers.put("Cookie", " " + cookie);//携带cookie
                         playUrl(url, headers);
+                        mController.setUrlTitle("视频地址："+url);
                         stopLoadWebView(false);
                     }
                 }
@@ -1685,6 +1686,7 @@ public class PlayFragment extends BaseLazyFragment {
                         String cookie = CookieManager.getInstance().getCookie(url);
                         if(!TextUtils.isEmpty(cookie))webHeaders.put("Cookie", " " + cookie);//携带cookie
                         playUrl(url, webHeaders);
+                        mController.setUrlTitle("视频地址："+url);
                         stopLoadWebView(false);
                     }
                 }
